b1c879500f9fd3014612bdffa6af74cb6e1aa980
Alessander <alessander.logistica@gmail.com>
2020-04-23 23:24:54 -0300

    create the service interfaces

---
+++ src/main/java/br/com/alessanderleite/app/services/ClienteService.java
@@ -0,0 +1,7 @@
+package br.com.alessanderleite.app.services;
+
+import br.com.alessanderleite.app.model.Cliente;
+
+public interface ClienteService extends CRUDService<Cliente, Integer>{
+
+}
---
+++ src/main/java/br/com/alessanderleite/app/services/LocalidadeService.java
@@ -0,0 +1,7 @@
+package br.com.alessanderleite.app.services;
+
+import br.com.alessanderleite.app.model.Localidade;
+
+public interface LocalidadeService extends CRUDService<Localidade, Integer>{
+
+}
