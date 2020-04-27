dca87147b0eda4695aa10bc7905713d9c4c50256
Alessander <alessander.logistica@gmail.com>
2020-04-26 22:06:31 -0300

    create the class named HistoricoController

---
+++ src/main/java/br/com/alessanderleite/app/web/controller/HistoricoController.java
@@ -0,0 +1,27 @@
+package br.com.alessanderleite.app.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alessanderleite.app.business.HistoricoBO;
+
+@RestController
+@RequestMapping("/historico")
+public class HistoricoController {
+
+	@Autowired
+	private HistoricoBO historicoBO;
+	
+	@GetMapping
+	public ResponseEntity<?> listaHistorico() {
+		return ResponseEntity.ok().body(historicoBO.listaHistoricos());
+	}
+	
+	public ResponseEntity<?> buscarHistoricoClienteId(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(historicoBO.buscarById(id));
+	}
+}
