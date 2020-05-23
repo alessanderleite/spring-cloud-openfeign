93b6f82dbcc14141ce916f2f5d295ec3ce0b99e2
Alessander <alessander.logistica@gmail.com>
2020-05-23 19:41:41 -0300

    create the configuration class for cache

--- src/main/java/br/com/alessanderleite/app/config/CacheConfiguration.java
+++ src/main/java/br/com/alessanderleite/app/config/CacheConfiguration.java
@@ -1,5 +1,33 @@
 package br.com.alessanderleite.app.config;
 
-public class CacheConfiguration {
+import java.util.concurrent.TimeUnit;
+
+import javax.cache.CacheManager;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.TouchedExpiryPolicy;
+
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfiguration implements JCacheManagerCustomizer {
+
+	@Override
+	public void customize(CacheManager cacheManager) {
+		cacheManager.createCache("cliente", configurar(new Duration(TimeUnit.MINUTES, 5)));
+		cacheManager.createCache("historico", configurar(new Duration(TimeUnit.MINUTES, 5)));
+		cacheManager.createCache("localidade", configurar(new Duration(TimeUnit.MINUTES, 5)));
+	}
 	
+	private MutableConfiguration<Object, Object> configurar(Duration duracao) {
+		MutableConfiguration<Object, Object> configTouch5seconds = new MutableConfiguration<>()
+				.setExpiryPolicyFactory(TouchedExpiryPolicy.factoryOf(duracao))
+				.setStoreByValue(false)
+				.setStatisticsEnabled(true)
+				.setManagementEnabled(true);
+		return configTouch5seconds;
+	}
 }
